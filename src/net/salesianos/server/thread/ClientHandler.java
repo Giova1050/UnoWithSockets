94df3030e531b4e668c28f13ad7d929d9374096d
Giovanni <usr3855@salesianos-lacuesta.net>
2025-02-09 13:52:21 +0000

    clientHandler created

--- src/net/salesianos/server/thread/ClientHandler.java
+++ src/net/salesianos/server/thread/ClientHandler.java
@@ -1,5 +1,51 @@
 package net.salesianos.server.thread;
 
-public class ClientHandler {
-    
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+import net.salesianos.server.UnoServer;
+
+public class ClientHandler implements Runnable {
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private List<String> hand = new ArrayList<>();
+
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+
+            for (int i = 0; i < 7; i++) {
+                hand.add(UnoServer.deck.drawCard());
+            }
+            sendMessage("Tus cartas: " + hand);
+
+            while (true) {
+                String input = in.readLine();
+                if (input == null || input.equalsIgnoreCase("salir")) {
+                    break;
+                }
+                sendMessage("Comando recibido: " + input);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void sendMessage(String message) {
+        out.println(message);
+    }
 }
