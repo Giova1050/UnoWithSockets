286a87144974ecb92bf68cd6113d7962442e6112
Giovanni <usr3855@salesianos-lacuesta.net>
2025-02-09 13:50:47 +0000

    Deck created

--- src/net/salesianos/client/Deck.java
+++ src/net/salesianos/client/Deck.java
@@ -1,5 +1,27 @@
 package net.salesianos.client;
 
+import java.util.*;
+
 public class Deck {
-    
+    private List<String> cards;
+
+    public Deck() {
+        cards = new ArrayList<>();
+        String[] colors = {"Rojo", "Azul", "Verde", "Amarillo"};
+        String[] values = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+        for (String color : colors) {
+            for (String value : values) {
+                cards.add(color + " " + value);
+                if (!value.equals("0")) {
+                    cards.add(color + " " + value);
+                }
+            }
+        }
+        Collections.shuffle(cards);
+    }
+
+    public String drawCard() {
+        return cards.isEmpty() ? "No hay cartas" : cards.remove(0);
+    }
 }
